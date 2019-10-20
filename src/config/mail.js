d33c853cc605cc3dd523489ffe0c365fe653ea5e
Diogo Naka <diogoshindhinaka@gmail.com>
2019-10-20 20:27:09 -0300

    Create the method and configure the email sending part using Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,26 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '79716a74d3a44b',
+    pass: 'e2182481c41ef1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Emailing Services
+ *
+ * PROD
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp)
+ * GMail -> Boring
+ *
+ * DEV
+ * Mailtrap (DEV)
+ */
