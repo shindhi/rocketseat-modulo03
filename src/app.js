f6a32f772150108146050481c27721d3d335599f
Diogo Naka <diogoshindhinaka@gmail.com>
2019-10-22 23:58:24 -0300

    Integrates (Sentry) for exception handling

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 // Configuracao do servidor express
 import express from 'express';
 import path from 'path';
-import routes from './routes';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 
-// Chama o arquivo
+import routes from './routes';
+import sentryConfig from './config/Sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   // Metodos
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -25,6 +33,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
